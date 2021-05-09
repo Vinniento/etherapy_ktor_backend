829946446de0385aa6936a73e7ce9a23a49d8a75
Vince <schauervincent@gmail.com>
2021-05-09 22:16:14 +0200

    basic routing works, host set, config set

--- src/main/kotlin/fh_campus/Application.kt
+++ src/main/kotlin/fh_campus/Application.kt
@@ -3,9 +3,30 @@ package fh_campus
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import fh_campus.plugins.*
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
-    }.start(wait = true)
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module(testing: Boolean = false) {
+   // configureRouting()
+
+    install(DefaultHeaders) {
+
+    }
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+            disableHtmlEscaping()
+        }
+    }
+
+    routing {
+        get("/"){
+            call.respondText("bladibla")
+        }
+    }
 }
